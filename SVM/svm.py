6dffbe9973c6d246c02e5fe8c4a4f2c90dc80abd
Federico <federico.taschin@studenti.unipd.it>
2019-10-03 16:14:59 +0200

    Refactored and bugfix

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,122 +1,105 @@
-import numpy, random , math
-from scipy . optimize import minimize
-import matplotlib . pyplot as plt
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+import random
+from scipy import optimize as opt
 
-numpy.random.seed(100)
+def kernelScalar(x, y):
+    return np.dot(x,y)
 
-classA = numpy.concatenate (
-    (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
-    numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
-classB = numpy.random.randn(20, 2) * 0.2 + [0.0, -0.5]
+def kernelPoly(x, y):
+    p = 4
+    return (np.dot(x,y) + 1)**p
 
-inputs = numpy.concatenate((classA, classB))
-targets = numpy.concatenate(
-    (numpy.ones(classA.shape[0]), 
-    -numpy.ones(classB.shape[0])))
+def zerofun(alphas):
+    global targets
+    return sum(np.multiply(alphas, targets))
 
-N = inputs.shape[0] # Number of rows (samples)
+def objective(alphas):
+    global N, targets, inputs
+    res = 0
+    for i in range(N):
+        for j in range(N):
+            res += alphas[i] * alphas[j] * targets[i] * targets[j] * kernel_matrix[i][j]
+    res /= 2
+    res -= sum(alphas)
+    return res
 
-permute = list (range(N))
-random.shuffle(permute) 
-inputs = inputs[permute, :]
-targets = targets[permute]
+def nonzero_alphas(alphas):
+    # vector of pairs (alpha, index) only for alphas > 10^-5
+    nonzero_alphas = [(a, i) for i, a in enumerate(alphas) if a > 10**-5]
+    return nonzero_alphas
 
-# PLOTTING
+# Returns the first support vector, which is the first one with 0 < alpha < C
+def valid_support_vector(valid_alphas, C):
+    global inputs
+    for alpha in valid_alphas:
+        if alpha[0] < C:
+            index = alpha[1]
+            vector = inputs[index]
+            return index, vector
 
-plt.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
-plt.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
-plt.axis('equal') # Force same scale in both axis
-plt.savefig('svmplot.pdf') # Save copy in pdf file
-
-
-
-# CONSTANTS
-
-dimension = 2
-C = 20
-dataset = [] # List of dataset elements
-
-def kernel(x, y):
-    p = 2
-    return (numpy.dot(x,y)+1)**p
-
-for i in range(N):
-    dataset.append([inputs[i], targets[i]])
-
-kernel_matrix = numpy.zeros([N,N])
-
-for i in range(N):
-    for j in range (N):
-        kernel_matrix[i][j] = kernel(dataset[i][0], dataset[j][0])
-
-# FUNCTIONS
-
-def zeroFun(alphas):
-
-    result = 0
-
-    for i, alpha in enumerate(alphas):
-        result += alpha * dataset[i][1]
-
-    return result
-
-def objective(vector):
-
-    result = 0
-
-    for i, alphai in enumerate(vector):
-        for j, alphaj in enumerate(vector):
-            result += alphai * alphaj * dataset[i][1] * dataset[j][1] * kernel_matrix[i][j]
-
-    result = result/2
-    result -= sum(vector)
-
-    return result
-
-def nonZeroAlpha(alphas):
-
-    nonzeroalphas = []
-    
-    for i, alpha in enumerate(alphas):
-        if (alpha > (10**(-5))):
-            nonzeroalphas.append([alpha, i])
-
-    return nonzeroalphas
-
-def bComputation(alphas):
-
+def computeB(valid_alphas, targets, kernel_matrix, C):
+    # Getting a support vector
+    support_index, support_vector = valid_support_vector(valid_alphas, C)
     b = 0
-    support_vector = []
-
-    for i, alpha in enumerate(alphas):
-        if (alpha[0] < C):
-            support_vector = [dataset[i], i]
-            break
-
-    for i, alpha in enumerate(alphas):
-        b += alpha[0] * dataset[i][1] * kernel_matrix[support_vector[1]][i] - dataset[support_vector[1]][1]
-
+    # alpha[0] is the value, alpha[1] its index
+    for alpha in valid_alphas:
+        i = alpha[1]
+        b += alpha[0] * targets[i] * kernel_matrix[support_index][i]
+    b -= targets[support_index]
     return b
 
-def indicator(alphas, s):
-
-    result = 0
-    b = bComputation(alphas)
+def indicator(s):
+    global valid_alphas, targets, kernel_matrix, b
+    ind = 0
+    # alpha[0] is the value, alpha[1] its index
+    for alpha in valid_alphas:
+        i = alpha[1]
+        ind += alpha[0] * targets[i] * kernelPoly(s, inputs[i])
+    ind -= b
+    return ind
     
-    for i, alpha in enumerate(alphas):
-        result += alpha[0] * dataset[i][1] * kernel(s, dataset[i][0]) - b
+C = 5
+# Creating dataset
+classA = np.concatenate((np.random.randn(10,2) * 0.2 + [1.5, 0.5], np.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
+classB = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
+inputs = np.concatenate((classA, classB))
+targets = np.concatenate((np.ones(classA.shape[0]), -np.ones(classB.shape[0])))
+N = inputs.shape[0] # Number of vector in our dataset
+permute = list(range(N))
+np.random.shuffle(permute)
+inputs = inputs [permute, :] # Nx2 matrix with vectors in the rows
+targets = targets[permute] # Array with classes of each vector
 
-    return result
-        
-# MAIN CODE
+kernel_matrix = np.zeros([N, N]) # Creating kernel matrix
+for i in range(N):
+    for j in range(N):
+        kernel_matrix[i][j] = kernelPoly(inputs[i], inputs[j])
 
-ret = minimize(objective, numpy.zeros(N), bounds=[(0, C) for b in range(N)], constraints={'type':'eq', 'fun':zeroFun})
+# Calling minimize
+alphas = opt.minimize(objective, np.zeros(N),
+        bounds=[(0, C) for b in range(N)],
+        constraints={'type':'eq', 'fun':zerofun})['x']
 
-alpha = nonZeroAlpha(ret['x'])
+# valid_alphas contains pairs (alpha, index)
+valid_alphas = nonzero_alphas(alphas)
 
-xgrid = numpy.linspace(-5, 5)
-ygrid = numpy.linspace(-4, 4)
-grid = numpy.array([[indicator(alpha, [x, y]) for x in xgrid ] for y in ygrid])
-plt.contour ( xgrid , ygrid , grid , (-1.0, 0.0, 1.0), colors = ('red', 'black', 'blue'), linewidths = (1, 3, 1))
+
+b = computeB(valid_alphas, targets, kernel_matrix, C)
+
+# Plotting everything
+plt.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
+plt.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
+plt.axis('equal')
+plt.savefig('svmplot.pdf')
+
+xgrid = np.linspace(-5, 5)
+ygrid = np.linspace(-4, 4)
+grid = np.array([[indicator(np.array([x, y])) for x in xgrid] for y in ygrid])
+
+plt.contour(xgrid, ygrid, grid,
+        (-1.0, 0.0, 1.0),
+        colors=('red', 'black', 'blue'),
+        linewidths=(1, 3, 1))
 plt.show()
-    
